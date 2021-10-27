98492b8df5310f58c529f1c7298c0d50d69396d1
Riccardo De Zen <riccardodezen98@gmail.com>
2021-10-27 12:46:35 +0200

    No clue, been too long

--- app/src/main/java/it/unipd/dezen/dezzilauncher/HomeActivity.java
+++ app/src/main/java/it/unipd/dezen/dezzilauncher/HomeActivity.java
@@ -1,14 +1,19 @@
 package it.unipd.dezen.dezzilauncher;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
@@ -22,14 +27,15 @@ import it.unipd.dezen.dezzilauncher.applist.AppRecyclerAdapter;
 public class HomeActivity extends AppCompatActivity {
 
     private ViewGroup appListLayout;
-    private ImageButton expandButton;
+    private ImageView expandArrow;
     private RecyclerView appListRecycler;
     private TextView dateText;
+    private BottomSheetBehavior bottomSheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.activity_main);
 
         // Initializing transition ---
         appListLayout = findViewById(R.id.list_layout);
@@ -37,14 +43,55 @@ public class HomeActivity extends AppCompatActivity {
 
         // Recycler initialization ---
         appListRecycler = findViewById(R.id.app_list_recycler);
-        appListRecycler.setLayoutManager(new LinearLayoutManager(this));
+        appListRecycler.setLayoutManager(new GridLayoutManager(this, 4));
         appListRecycler.setAdapter(new AppRecyclerAdapter(this));
 
-        // Button for list display ---
-        expandButton = findViewById(R.id.expand_app_list_button);
-        expandButton.setOnClickListener(view -> {
-            expandList();
+        // Bottom sheet init ---
+        expandArrow = findViewById(R.id.expand_app_list_arrow);
+        bottomSheet = BottomSheetBehavior.from(findViewById(R.id.bottom_sheet));
+        bottomSheet.addBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_EXPANDED:
+                        expandList();
+                        break;
+                    case BottomSheetBehavior.STATE_COLLAPSED:
+                        collapseList();
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+                appListLayout.setAlpha(slideOffset);
+            }
         });
+
+    }
+
+    /**
+     * Whenever we receive a new intent while already active, we may want to close the app list.
+     * We don't want to if we were just put out of focus (we need to preserve the user experience).
+     *
+     * @param intent The Intent that started this Activity.
+     */
+    @Override
+    protected void onNewIntent(Intent intent) {
+        super.onNewIntent(intent);
+        if (isListExpanded())
+            collapseList();
+        Log.e(this.getClass().getName(), intent.getFlags() + "");
+    }
+
+    /**
+     * When the Activity is paused we detect that we went out of focus.
+     */
+    @Override
+    protected void onPause() {
+        super.onPause();
     }
 
     /**
@@ -56,10 +103,15 @@ public class HomeActivity extends AppCompatActivity {
         updateDate();
     }
 
+    /**
+     * When the back button is pressed we collapse the bottom sheet
+     */
     @Override
     public void onBackPressed() {
-        if (isListExpanded())
-            collapseList();
+        if (bottomSheet.getState() == BottomSheetBehavior.STATE_EXPANDED)
+            bottomSheet.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        else
+            super.onBackPressed();
     }
 
     /**
@@ -73,16 +125,16 @@ public class HomeActivity extends AppCompatActivity {
      * Make button invisible and list visible.
      */
     private void expandList() {
-        expandButton.setVisibility(View.GONE);
-        appListLayout.setVisibility(View.VISIBLE);
+        expandArrow.setVisibility(View.INVISIBLE);
+        appListLayout.setAlpha(1);
     }
 
     /**
      * Make button visible and list invisible.
      */
     private void collapseList() {
-        appListLayout.setVisibility(View.GONE);
-        expandButton.setVisibility(View.VISIBLE);
+        appListLayout.setAlpha(0);
+        expandArrow.setVisibility(View.VISIBLE);
     }
 
     /**
